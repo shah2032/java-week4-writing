90874eed6692fead07ee63ad64c1aa14e3f751f4
shah2032 <tejal941982@gmail.com>
2023-04-06 13:05:43 +0000

    My week 4 writing homework

---
+++ src/homework_week_8/Programme_8_SymbolTriangle.java
@@ -0,0 +1,30 @@
+package homework_week_8;
+
+import java.util.Scanner;
+
+/**
+ * Display right angle triangle of @ using nested for loops
+ * @
+ * @@
+ * @@@
+ * @@@@
+ * @@@@@
+ */
+public class Programme_8_SymbolTriangle {
+    public static void symbolTriangle(int n){
+        for (int i = 0; i <= n; i++){
+            System.out.println(i);
+        }
+        System.out.println(" ");
+
+    }
+
+    public static void main(String[] args) {
+        //Scanner declaration for reading input from console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input number of rows: ");
+        symbolTriangle(scanner.nextInt());
+        //closing the scanner object
+        scanner.close();
+    }
+}
---
+++ src/programme_21_abstractexample/Shape.java
@@ -0,0 +1,13 @@
+package programme_21_abstractexample;
+
+/**
+ * /**
+ *  * 1. Create below Shape class which is abstract
+ *  */
+
+public abstract class Shape {
+
+
+    public abstract void draw();
+
+}
